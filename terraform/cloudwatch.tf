815d9dcd16cf417ebe71ed6fb75354d0b6bf1dfb
Jake Watkins <jwatkins@mozilla.com>
2018-06-19 12:04:35 -0700

    Fix cron format

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -3,7 +3,7 @@ resource "aws_cloudwatch_event_rule" "nightly" {
   name        = "schedule_timeline_repo_mirror_sync"
   description = "Triggers the Timeline Repo lambda nightly"
   # cron nightly around 2am PST/9am UTC (aka mozilla time)
-	schedule_expression = "cron(0 9 * * *)"
+	schedule_expression = "cron(0 9 * * ? *)"
   is_enabled = true
 }
 
